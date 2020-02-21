fd735610e0ec831e9cf1922c24b778bbdb8bb286
Matt Pahuta <mhpahuta@gmail.com>
2020-02-21 10:00:10 -0800

    initial html, css, and jquery

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,21 @@
 console.log("Scripts connected!")
+
+// Check off specific todos by clicking
+// $("li").click(function() {
+//   if ($(this).css("color") === "rgb(128, 128, 128)"){
+//     $(this).css({
+//       color: "black",
+//       textDecoration: "none"
+//     });
+//   } else {
+//     $(this).css({
+//       color: "gray",
+//       textDecoration: "line-through"
+//     });
+//   }
+// });
+
+// Single line, streamlined version of above logic
+$("li").click(function() {
+  $(this).toggleClass("completed");
+})
